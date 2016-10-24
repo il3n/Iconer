b12480eba4178805f699fd611616cde4807e66eb
lijun <lijun@leedeMacBook-Pro.local>
2016-10-24 10:35:48 +0800

    init

---
+++ Iconer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Iconer
+//
+//  Created by lijun on 16/10/24.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
